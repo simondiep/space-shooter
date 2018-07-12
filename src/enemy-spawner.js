fa56de6ba30fda2ba5227d8f37ac30c217107e7b
Simon Diep <simondiep1@gmail.com>
2018-07-11 20:20:08 -0500

    Create enemy base function

--- src/enemy-spawner.js
+++ src/enemy-spawner.js
@@ -14,70 +14,58 @@ const MAX_Y_SPAWN = CANVAS_HEIGHT - 30;
 export function spawnHearty() {
   const size = getRandomNumber(20, 30) + Math.floor(getTimeAliveInSeconds() / 15);
   const health = Math.floor(size / 5);
-  addEnemy({
-    x: CANVAS_WIDTH + 10,
-    y: getRandomNumber(0, MAX_Y_SPAWN),
-    vx: -getRandomNumber(2, 4),
-    vy: 0,
-    size,
-    health,
-    score: health,
-    recentlyDamaged: false,
-    turnsToDisplayDamage: 0,
-    hitByProjectiles: [],
-    onHit: self => {
-      self.size += 5;
-    },
-    images: {
-      normal: heartyImage,
-      damaged: heartyDamagedImage,
-    },
-  });
+  addEnemy(
+    createEnemy({
+      vx: -getRandomNumber(2, 4),
+      size,
+      health,
+      score: health,
+      onHit: self => {
+        self.size += 5;
+      },
+      images: {
+        normal: heartyImage,
+        damaged: heartyDamagedImage,
+      },
+    }),
+  );
 }
 
 export function spawnSpeedster() {
   const size = getRandomNumber(15, 30);
   const health = Math.floor(size / 15) + Math.floor(getTimeAliveInSeconds() / 20);
-  addEnemy({
-    x: CANVAS_WIDTH + 10,
-    y: getRandomNumber(0, MAX_Y_SPAWN),
-    vx: -getRandomNumber(6, 9) - Math.floor(getTimeAliveInSeconds() / 20),
-    vy: 0,
-    size,
-    health,
-    score: health * 2,
-    recentlyDamaged: false,
-    turnsToDisplayDamage: 0,
-    hitByProjectiles: [],
-    onHit: self => {
-      self.vx -= 2;
-    },
-    images: {
-      normal: speedsterImage,
-      damaged: speedsterDamagedImage,
-    },
-  });
+  addEnemy(
+    createEnemy({
+      vx: -getRandomNumber(6, 9) - Math.floor(getTimeAliveInSeconds() / 20),
+      size,
+      health,
+      score: health * 2,
+      onHit: self => {
+        self.vx -= 2;
+      },
+      images: {
+        normal: speedsterImage,
+        damaged: speedsterDamagedImage,
+      },
+    }),
+  );
 }
 
 export function spawnSpaceInvader() {
   const size = getRandomNumber(20, 40) + Math.floor(getTimeAliveInSeconds() / 20);
   const health = Math.floor(size / 10);
-  addEnemy({
-    x: CANVAS_WIDTH + 10,
-    y: getRandomNumber(0, MAX_Y_SPAWN),
-    vx: -getRandomNumber(3, 6),
-    vy: 0,
-    size,
-    health,
-    score: health,
-    recentlyDamaged: false,
-    turnsToDisplayDamage: 0,
-    hitByProjectiles: [],
-    images: {
-      normal: spaceInvaderImage,
-      damaged: spaceInvaderDamagedImage,
-    },
-  });
+  addEnemy(
+    createEnemy({
+      vx: -getRandomNumber(3, 6),
+      size,
+      health,
+      score: health,
+      images: {
+        normal: spaceInvaderImage,
+        damaged: spaceInvaderDamagedImage,
+      },
+    }),
+  );
 }
 
 export function spawnSpaceInvaderWave() {
@@ -88,97 +76,100 @@ export function spawnSpaceInvaderWave() {
 }
 
 export function spawnBoss() {
-  const bossType = getRandomNumber(3, 3);
+  const bossType = getRandomNumber(1, 3);
   switch (bossType) {
     case 1: // Space invader
       const invaderSize = getRandomNumber(100, 200) + Math.floor(getTimeAliveInSeconds() / 2);
       const invaderHealth = Math.floor(invaderSize / 10) * 2;
-      addEnemy({
-        x: CANVAS_WIDTH + 200,
-        y: getRandomNumber(0, MAX_Y_SPAWN),
-        vx: -getRandomNumber(1, 2),
-        vy: 0,
-        size: invaderSize,
-        health: invaderHealth,
-        score: invaderHealth * 3,
-        recentlyDamaged: false,
-        turnsToDisplayDamage: 0,
-        hitByProjectiles: [],
-        images: {
-          normal: spaceInvaderImage,
-          damaged: spaceInvaderDamagedImage,
-        },
-      });
+      addEnemy(
+        createEnemy({
+          x: CANVAS_WIDTH + 200,
+          vx: -getRandomNumber(1, 2),
+          size: invaderSize,
+          health: invaderHealth,
+          score: invaderHealth * 3,
+          images: {
+            normal: spaceInvaderImage,
+            damaged: spaceInvaderDamagedImage,
+          },
+        }),
+      );
       break;
     case 2: // speedster
       const speedsterSize = getRandomNumber(75, 100) + Math.floor(getTimeAliveInSeconds() / 5);
       const speedsterHealth = Math.floor(speedsterSize / 7);
-      addEnemy({
-        x: CANVAS_WIDTH + 200,
-        y: getRandomNumber(0, MAX_Y_SPAWN),
-        vx: -getRandomNumber(3, 5),
-        vy: 0,
-        size: speedsterSize,
-        health: speedsterHealth,
-        score: speedsterHealth * 5,
-        recentlyDamaged: false,
-        turnsToDisplayDamage: 0,
-        hitByProjectiles: [],
-        onHit: self => {
-          self.vx--;
-        },
-        images: {
-          normal: speedsterImage,
-          damaged: speedsterDamagedImage,
-        },
-      });
+      addEnemy(
+        createEnemy({
+          x: CANVAS_WIDTH + 200,
+          vx: -getRandomNumber(3, 5),
+          size: speedsterSize,
+          health: speedsterHealth,
+          score: speedsterHealth * 5,
+          onHit: self => {
+            self.vx--;
+          },
+          images: {
+            normal: speedsterImage,
+            damaged: speedsterDamagedImage,
+          },
+        }),
+      );
       break;
     case 3: // hearty
       const heartySize = getRandomNumber(50, 60) + Math.floor(getTimeAliveInSeconds() / 5);
       const heartyHealth = Math.floor(heartySize / 2);
-      addEnemy({
-        x: CANVAS_WIDTH + 10,
-        y: getRandomNumber(0, MAX_Y_SPAWN),
-        vx: -getRandomNumber(1, 3),
-        vy: 0,
-        size: heartySize,
-        health: heartyHealth,
-        score: heartyHealth * 5,
-        recentlyDamaged: false,
-        turnsToDisplayDamage: 0,
-        hitByProjectiles: [],
-        onHit: self => {
-          self.size += 5;
-        },
-        images: {
-          normal: heartyImage,
-          damaged: heartyDamagedImage,
-        },
-      });
+      addEnemy(
+        createEnemy({
+          x: CANVAS_WIDTH + 200,
+          vx: -getRandomNumber(1, 3),
+          size: heartySize,
+          health: heartyHealth,
+          score: heartyHealth * 5,
+          onHit: self => {
+            self.size += 5;
+          },
+          images: {
+            normal: heartyImage,
+            damaged: heartyDamagedImage,
+          },
+        }),
+      );
   }
 }
 
 export function spawnAsteroid() {
   const size = getRandomNumber(20, 40) + Math.floor(getTimeAliveInSeconds() / 25);
   const health = size;
-  addEnemy({
-    x: CANVAS_WIDTH + 10,
-    y: getRandomNumber(0, MAX_Y_SPAWN),
-    vx: -getRandomNumber(3, 6),
-    vy: getRandomNumber(-1, 1),
-    size,
-    health,
-    score: 0,
-    recentlyDamaged: false,
-    turnsToDisplayDamage: 0,
-    hitByProjectiles: [],
-    images: {
-      normal: asteroidImage,
-      damaged: asteroidImage,
-    },
-  });
+  addEnemy(
+    createEnemy({
+      vx: -getRandomNumber(3, 6),
+      vy: getRandomNumber(-1, 1),
+      size,
+      health,
+      images: {
+        normal: asteroidImage,
+        damaged: asteroidImage,
+      },
+    }),
+  );
 }
 
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function createEnemy(overrides) {
+  return {
+    x: CANVAS_WIDTH + 10,
+    y: getRandomNumber(0, MAX_Y_SPAWN),
+    vx: 0,
+    vy: 0,
+    size: 30,
+    health: 1,
+    score: 1,
+    recentlyDamaged: false,
+    turnsToDisplayDamage: 0,
+    hitByProjectiles: [],
+    ...overrides,
+  };
 }
